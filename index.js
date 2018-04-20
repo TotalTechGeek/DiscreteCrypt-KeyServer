880dae81e8a0509152b0e5d5b1f65d926f9e32b3
TotalTechGeek <TotalTechGeek@users.noreply.github.com>
2018-04-20 18:53:37 -0500

    Fix

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const config = require('./config.js').data
 
 if(process.env.SECRET_SECURITY_KEY)
 {
-    config.security.key = process.env.SECRET_KEY;
+    config.security.key = process.env.SECRET_KEY.toString();
+    console.log(config.security.key)
 }
 
 const compression = require('compression')
